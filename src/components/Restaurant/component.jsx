e04a407079c72d7e16ba55dad79d2835ed2689ec
anton_vakhrushin <avvakhrushin@gmail.com>
2023-10-19 00:06:50 +0300

    fix reviews + delete mock data

--- src/components/Restaurant/component.jsx
+++ src/components/Restaurant/component.jsx
@@ -1,8 +1,8 @@
 import classNames from 'classnames';
 import { Menu } from '../Menu/component';
 import { ReviewForm } from '../ReviewForm/component';
-import { Reviews } from '../Reviews/component';
 import styles from './styles.module.scss';
+import { ReviewsContainer } from '../Reviews/container';
 
 export const Restaurant = ({ restaurant }) => {
     return (
@@ -11,7 +11,7 @@ export const Restaurant = ({ restaurant }) => {
             <h2>Menu</h2>
             <Menu className={classNames(styles.menu)} dishIds={restaurant.menu} />
             <h2>Reviews</h2>
-            <Reviews className={classNames(styles.reviews)} reviewIds={restaurant.reviews} />
+            <ReviewsContainer className={classNames(styles.reviews)} reviewIds={restaurant.reviews} />
             <h2>Leave a review</h2>
             <ReviewForm />
         </div>
--- src/components/Review/container.jsx
+++ src/components/Review/container.jsx
@@ -1,16 +1,9 @@
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { selectReviewById } from '../../redux/entities/reviews/selectors';
 import { Review } from './component';
-import { useEffect } from 'react';
-import { getUsers } from '../../redux/entities/users/thunks/get-users';
 
 export const ReviewContainer = ({ reviewId, ...props }) => {
     const review = useSelector(state => selectReviewById(state, reviewId));
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(getUsers());
-    }, []);
 
     return <Review review={review} {...props} />;
 };
